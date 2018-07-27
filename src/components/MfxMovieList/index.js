69ce2806f43f69d406c4b53a72d46cb3b6accf88
Matt Johnson <matt@mattpjohnson.com>
2018-07-27 18:17:20 -0400

    feat(MfxMovieList): Add details

--- src/components/MfxMovieList/index.js
+++ src/components/MfxMovieList/index.js
@@ -18,7 +18,13 @@ class MfxMovieListItem extends React.Component {
 
         return (
             <div className="mfx-movie-list-item">
-                <img alt={`${movie.Title} poster`} className="mfx-movie-list-item__poster" src={movie.Poster} />
+                <div className="mfx-movie-list-item__poster-wrapper">
+                    <img alt={`${movie.Title} poster`} className="mfx-movie-list-item__poster" src={movie.Poster} />
+                    <div className="mfx-movie-list-item__details">
+                        <div className="mfx-movie-list-item__details__title">{movie.Title}</div>
+                        <div className="mfx-movie-list-item__details__plot">{movieDetails.Plot}</div>
+                    </div>
+                </div>
                 <p className="mfx-movie-list-item__title">{movie.Title}</p>
                 <span className="mfx-movie-list-item__genre">{movieDetails.Genre}</span>
             </div>
